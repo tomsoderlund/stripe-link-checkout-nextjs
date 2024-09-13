f75a5db5496474161732eb04f5fdcdd0c0d419b8
Tom Söderlund <tom@tomsoderlund.com>
2024-09-13 09:12:13 +0200

    Cancel & Success

---
+++ pages/cancel.tsx
@@ -0,0 +1,7 @@
+export default function Cancel() {
+  return (
+    <main>
+      <h1>Cancel</h1>
+    </main>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-const title = 'Stripe Link Checkout for Next.js';
+import Link from 'next/link'
+
+import packageJson from "../package.json";
+
 
 export default function Home() {
   return (
     <main>
-      <h1>{title}</h1>
+      <h1>{packageJson.description}</h1>
+      <div className='flex'>
+        <Link className='button' href='/buy-once'>
+          Buy Once
+        </Link>
+        <Link className='button' href='/buy-subscription'>
+          Buy Subscription
+        </Link>
+      </div>
     </main>
   );
 }
